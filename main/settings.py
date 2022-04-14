c2eef09f7c7753dc4459fd9f9368ab29d2743079
malika65 <malika.satimbaeva@iaau.edu.kg>
2022-04-14 18:37:06 +0600

    fix settings

--- main/settings.py
+++ main/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'whitenoise.runserver_nostatic'
+    'whitenoise.runserver_nostatic',
     'rest_framework',
     'bereket',
 ]
@@ -55,7 +55,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware'
+    'main.middleware.open_access_middleware'
 ]
 
 ROOT_URLCONF = 'main.urls'
